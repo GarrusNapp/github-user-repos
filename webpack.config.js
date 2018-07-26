fa5062338246fa4cbcc8f28fa2ca32c42f96a775
Garrus Napp <garrusnapp@gmail.com>
2018-07-26 15:27:50 +0200

    Project structure, dependencies, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  entry:"./js/components/App.js",
+  output: { filename: "./build/out.js"},
+  mode: "development", watch: true,
+  devtool:"#eval-source-map",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015", "stage-2", "react"]
+        }
+      }
+    }]
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./",
+    port: 3001
+  }
+}
